3c92c5c2e028cfd8e6b4acea8e708c1f025d9d21
Juan Vargas <62524570+juanpivl@users.noreply.github.com>
2023-01-19 19:35:52 -0700

    al dia

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link :to="{name: 'counter'}">Counter</router-link>  
+    <router-link :to="{name: 'counter'}">Counter</router-link> | 
+    <router-link :to="{name: 'users'}">Usuarios</router-link>  
+
   </nav>
-  <router-view v-slot="{Component}">
+  <router-view v-slot="{Component,route}">
     <keep-alive>
-      <component :is="Component"/>
+      <component :is="Component" :key="route.name"/>
     </keep-alive>
   </router-view>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -21,7 +21,15 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "conter" */ '../views/CounterView.vue')
+    component: () => import(/* webpackChunkName: "counter" */ '../views/CounterView.vue')
+  },
+  {
+    path: '/users',
+    name: 'users',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "users" */ '../views/Users.vue')
   }
 ]
 
